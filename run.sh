21c673f2846cfd2f9f87eb7aeeb62aebc4e2f9f8
chanux <chanuka@thinkcube.com>
2017-01-12 13:21:17 +0800

    Adds minor tweak to allow lazy runs without data volume mounted

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -o errexit
 
 if [ "$(ls -A /data/raneto 1&>/dev/null )" ];then
     echo "==> Raneto content found. Continuing with it."
 else
     echo "==> No content found. Using default content."
+    if [ ! -d /data/raneto ];then
+        mkdir -p /data/raneto
+    fi
     cp -r /raneto/content /data/raneto
 fi
 
